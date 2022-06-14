5e690473d2691f7a72ea6b10e40f3c5ede94e065
Flais <fabiotrujillogaribay81@gmail.com>
2022-06-14 14:56:34 -0500

    Con mundo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,7 @@
 //Mi clase en Java
 public class HolaMundo {
     public static void main (String args[]) {
-        System.out.println("Adiós ");
+        System.out.println("Adiós");
+        System.out.println("Mundo");
     }
 }
